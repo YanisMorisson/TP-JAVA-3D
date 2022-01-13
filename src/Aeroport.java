6a38405db96e678670ab82fc082ea7167eaab4a6
YanisMorisson <44526756+YanisMorisson@users.noreply.github.com>
2022-01-13 10:49:14 +0100

    tt

--- src/Aeroport.java
+++ src/Aeroport.java
@@ -3,11 +3,11 @@ public class Aeroport {
     private String IATA;
     private String Name;
     private String country;
-    private double latitude;
-    private double longitude;
+    private Double latitude;
+    private Double longitude;
 
 
-    public aeroport( String IATA , String Name , String country , double latitude , double longitude) {
+    public Aeroport( String IATA , String Name , String country , Double latitude , Double longitude) {
         this.IATA = IATA;
         this.Name = Name;
         this.country = country;
@@ -19,17 +19,17 @@ public class Aeroport {
         return IATA;
     }
 
-    public double getLatitude() {
+    public Double getLatitude() {
         return latitude;
     }
 
-    public double getLongitude() {
+    public Double getLongitude() {
         return longitude;
     }
 
     @Override
     public String toString() {
-        return "aeroport{" +
+        return "Aeroport{" +
                 "IATA='" + IATA + '\'' +
                 ", Name='" + Name + '\'' +
                 ", country='" + country + '\'' +
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main (String[] args){
+        Aeroport CharlesDeGaulle = new Aeroport("CDG","Charles de Gaulle", "France", 49.01,20.01);
+    }
+
+}
