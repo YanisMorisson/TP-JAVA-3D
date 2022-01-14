8c050c204022ca7e3e3d1bc9eae1fd06227ff6fd
YanisMorisson <44526756+YanisMorisson@users.noreply.github.com>
2022-01-14 17:32:08 +0100

    tt

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main (String[] args){
         Aeroport CharlesDeGaulle = new Aeroport("CDG","Charles de Gaulle", "France", 49.01,20.01);
+        World w = new World("/Users/sinay/ENSEA/3D/TP/JAVA/airport-codes_no_comma.csv");
+
         System.out.println (CharlesDeGaulle);
     }
 
-    World w = new World("/Users/sinay/ENSEA/3D/TP/JAVA/airport-codes_no_comma.csv");
 
 }
--- src/World.java
+++ src/World.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import static java.lang.Math.*;
 
 public class World {
     ArrayList<Aeroport> Aeroportlist;
@@ -37,5 +38,39 @@ public class World {
             }
 
         }
+
+    public Aeroport findByCode(String code){
+        for(Aeroport aero : Aeroportlist){
+            if(aero.getIATA().equals(code)){
+                return aero;
+            }
+        }
+        System.out.println("Le code IATA ne correspond à aucun aeroport !!! ");
+        return null;
     }
+
+    public Aeroport findNearestAirport(double lon, double lat){ // on utilise la lon et lat de notre position
+        double dist = distance(lon,lat,Aeroportlist.get(0).getLongitude(),Aeroportlist.get(0).getLatitude()); // on fait un calcul de dist entre notre pos et le premier aeroport de la list
+        Aeroport nearestAirport = null; // mem tampon
+        double index = 0;
+
+        for(Aeroport a : Aeroportlist){  // Parcourir la liste pour comparer les distances pour chaque aeroport un par un et garder le plus proche
+            double lon1 = a.getLongitude();
+            double lat1 = a.getLatitude();
+            double d = distance(lon,lat,lon1, lat1);
+            if(d < dist){ // Si la distance aeroport b est plus petite que aeroport a
+                nearestAirport = a;      // le nouveau aeroport devient le plus proche
+                dist = d;  // la distance est actualisée
+            }
+        }
+        return nearestAirport; // on retourne l'aeroport le plus proche
+    }
+
+    public double distance(double lo1, double la1, double lo2, double la2){
+        double norme = pow(la2 - la1, 2) + pow((lo2 - lo1) * cos(toRadians((la2 + la1) / 2)), 2); // equation page 2
+
+        return norme;
+    }
+
+}
 
