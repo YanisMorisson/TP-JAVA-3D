6bfd725ceff0c05c37f4578ca70584d1bb3c5e56
YanisMorisson <44526756+YanisMorisson@users.noreply.github.com>
2022-01-21 12:00:12 +0100

    tt

--- src/Earth.java
+++ src/Earth.java
@@ -10,30 +10,5 @@ import javafx.scene.transform.Rotate;
 import java.util.ArrayList;
 
 public class Earth extends Group {
-    private Rotate ry = new Rotate();
-    private Sphere sph = new Sphere();
-    ArrayList<Sphere> yellowSphere = new ArrayList<>();
-    PhongMaterial material = new PhongMaterial();
-    //private Scene ihm = new Scene();
 
-    public Earth() {
-        super();
-        Image im = new Image("/Users/sinay/ENSEA/3D/TP/JAVA/earth_lights_4800.png");
-        sph.setRadius(300);
-        sph.getTransforms().add(ry);
-        material.setDiffuseMap(im);
-        sph.setMaterial(material);
-        this.getChildren().add(sph);
-
-
- /*   }
-    AnimationTimer animationTimer = new AnimationTimer() {
-        double angle = 0;
-        @Override
-        public void handle(long time) {
-            ry.setAngle();
-        }
-    };
-            animationTimer.start();*/
-    }
 }
